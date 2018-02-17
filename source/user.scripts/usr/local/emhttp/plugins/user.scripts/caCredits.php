e6607a4178e02544479df5ff1595e3e4e70f8348
Squidly271 <azawadzki@arrowgames.com>
2018-02-16 21:46:05 -0500

    More intuitive UI changes

--- source/user.scripts/usr/local/emhttp/plugins/user.scripts/caCredits.php
+++ source/user.scripts/usr/local/emhttp/plugins/user.scripts/caCredits.php
@@ -1,25 +1,9 @@
 <?
 ###############################################################
 #                                                             #
-# Community Applications copyright 2015-2016, Andrew Zawadzki #
+# Community Applications copyright 2016-2018, Andrew Zawadzki #
 #                                                             #
 ###############################################################
-
-function getLineCount($directory) {
-  global $lineCount;
-
-  $allFiles = array_diff(scandir($directory),array(".",".."));
-  foreach ($allFiles as $file) {
-    if (is_dir("$directory/$file")) {
-      getLineCount("$directory/$file");
-      continue;
-    }
-    $extension = pathinfo("$directory/$file",PATHINFO_EXTENSION);
-    if ( $extension == "sh" || $extension == "php" || $extension == "page" ) {
-      $lineCount = $lineCount + count(file("$directory/$file"));
-    }
-  }
-}
 
 $caCredits = "
     <center><table align:'center'>
@@ -30,11 +14,9 @@ $caCredits = "
       </tr>
     </table></center>
     <br>
-    <center><em><font size='1'>Copyright 2016-2017 Andrew Zawadzki</font></em></center>
+    <center><em><font size='1'>Copyright 2016-2018 Andrew Zawadzki</font></em></center>
     <center><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7M7CBCVU732XG' target='_blank'><img src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'></a></center>
     <br><center><a href='https://forums.lime-technology.com/topic/48286-plugin-ca-user-scripts/' target='_blank'>Plugin Support Thread</a></center>
   ";
-  getLineCount("/usr/local/emhttp/plugins/user.scripts");
-  $caCredits .= "<center>$lineCount Lines of code and counting!</center>";
   $caCredits = str_replace("\n","",$caCredits);
 ?>
