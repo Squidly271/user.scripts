b8106c900c75d4faf6758574f97bde8cf36c2e24
l4t3b0 <l4t3b0@gmail.com>
2020-08-19 21:10:55 +0200

    Update startScript.sh

--- source/user.scripts/usr/local/emhttp/plugins/user.scripts/startScript.sh
+++ source/user.scripts/usr/local/emhttp/plugins/user.scripts/startScript.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Script location: <b>$1</b>"
+export HOME=$(grep $(whoami) /etc/passwd | cut -d: -f 6)
+source ${HOME}/.bashrc
 echo "<font color='red'>Note that closing this window will abort the execution of this script</font>"
 "$1" "$2" 2>&1
