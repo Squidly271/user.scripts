08f51b61029cc2a92817b5177056d82f87e31479
Squidly271 <unraidsquid@gmail.com>
2023-09-29 12:23:43 -0400

    run update_cron

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -3,5 +3,5 @@
 mkdir -p "/tmp/GitHub/user.scripts/source/user.scripts/usr/local/emhttp/plugins/user.scripts/"
 
 cp /usr/local/emhttp/plugins/user.scripts/* /tmp/GitHub/user.scripts/source/user.scripts/usr/local/emhttp/plugins/user.scripts -R -v -p
-
+find . -maxdepth 9999 -noleaf -type f -name "._*" -exec rm -v "{}" \;
 
